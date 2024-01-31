56bfd615bf06d070df9bef5b1a01aa9bd74b45ce
Jonathan Flunger <jonathan.flunger@ucdconnect.ie>
2024-01-31 03:01:04 +0100

    added continous integration

--- test/test_detector_fitting.py
+++ test/test_detector_fitting.py
@@ -1,7 +1,16 @@
 import sys
 sys.path.append(r"C:\Users\jonat\OneDrive - Universität Wien\4_COSMOS Seminar\gammaforge")
 from gammaforge.utils.import_ini import load_config
+from gammaforge.analysis.fitting import fit_all, load_events
 
 def test_load_config():
     peaks, dates, dir = load_config(r"C:\Users\jonat\OneDrive - Universität Wien\4_COSMOS Seminar\gammaforge\example-scripts\config.ini")
     assert len(peaks["Ba133"]) == 3
+
+def test_fit_all():
+    dates, peaks, dir = load_config(r"C:\Users\jonat\OneDrive - Universität Wien\4_COSMOS Seminar\gammaforge\example-scripts\config.ini")
+    events = load_events(dates, dir)
+    df = fit_all("Ba133", events, peaks)
+    assert len(df) == 4
+
+    
